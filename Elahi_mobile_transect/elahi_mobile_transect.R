d1428626997265a05bf2d6a810eabf582443b7c0
Robin Elahi <elahi.robin@gmail.com>
2016-05-25 20:45:25 -0700

    fixed up treatment info in mobile transect data

--- Elahi_mobile_transect/elahi_mobile_transect.R
+++ Elahi_mobile_transect/elahi_mobile_transect.R
@@ -21,6 +21,8 @@ glimpse(dat)
 # Examine data
 range(dat$length) # note that 0.01 indicates that size data were not available, but the animal was observed
 
+##### FIX SPECIES CODES #####
+
 # Deal with species codes
 sppL <- sort(unique(dat$code))
 
@@ -64,9 +66,51 @@ sppMetaData <- data.frame("code" = sort(unique(code2)),
 dat$code <- code2
 head(dat)
 
-# Check dates
-with(dat, table(comm.date, transect))
+##### ADD INFO ON URCHIN REMOVALS #####
 
-write.csv(dat, 'Elahi_mobile_transect/output/elahi_mobile_tran_data.csv')
+mobDat <- read_csv("Elahi_mobile_quadrat/output/elahi_mobile_quad_data.csv")
+glimpse(mobDat)
+
+mobDat2 <- mobDat %>% filter(era == "Modern.R") %>%
+  select(transect, comm.date, urch, removals) 
+head(mobDat2)
+
+urchinInfo <- mobDat %>% filter(era == "Modern.R") %>%
+  select(transect, urch) %>% 
+  distinct(transect)
+
+# Bind treatment info to transect data
+dat2 <- left_join(dat, urchinInfo, by = "transect")
+
+# Add info for urchin removal dates
+mobDat %>% filter(era == "Modern.R") %>%
+  select(comm.date, removals) %>% 
+  distinct(comm.date) 
+
+dat2 %>% filter(urch == "u_rem" & code == "SFR") %>% 
+  group_by(site, transect, code, comm.date) %>%
+  summarise(n = n()) %>% ungroup() %>% 
+  group_by(comm.date) %>% 
+  summarise(urchins = mean(n, na.rm = TRUE)) %>% 
+  ggplot(., aes(comm.date, urchins)) + geom_line()
+
+# Grazer removals began on 18 April 2009 and 
+# continued every two weeks until 24 January 2010. 
+# (PLOS1 paper)
+sort(unique(dat2$comm.date))
+
+crap <- ymd(unique(dat2$comm.date))
+crap > ymd(20090417)
+
+dat3 <- dat2 %>% 
+  mutate(comm.date = ymd(comm.date), 
+         removals = ifelse(
+    comm.date > ymd(20090417) & comm.date < ymd(20100124), 
+    "yes", "no"))
+
+with(dat3, table(removals))
+
+
+write.csv(dat3, 'Elahi_mobile_transect/output/elahi_mobile_tran_data.csv')
 unique(dat$surveyor)
 
